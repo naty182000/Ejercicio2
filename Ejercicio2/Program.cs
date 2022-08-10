77dfe09a545da9214044a766f3e271bea2c884ba
DESKTOP-7MTEE4C\natal <nataly182000@gmail.com>
2022-08-10 17:23:15 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[5,5];
+
+            for(int filas = 0; filas < matriz.GetLength(0); filas++)
+            {
+                for(int colum =0; colum < matriz.GetLength(1); colum++)
+                {
+                    matriz[filas, colum] = (filas + 1) * (colum + 1);
+
+                }
+
+            }
+            for (int filas = 0; filas < matriz.GetLength(0); filas++)
+            {
+                for (int colum = 0; colum < matriz.GetLength(1); colum++)
+                {
+                    Console.WriteLine(matriz[filas, colum]);
+
+                }
+
+            }
+        }
+    }
+}
